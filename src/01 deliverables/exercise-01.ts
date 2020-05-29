ba222f4683355afc60f4ac678327b400fde76c07
Francisco J. Martínez <fjmartinez@unicaja.es>
2020-05-29 20:22:41 +0200

    Inicialización.

---
+++ src/01 deliverables/exercise-01.ts
@@ -0,0 +1,30 @@
+console.log("************** DELIVERABLE 01 *********************");
+
+//1. Array operations
+//Head
+Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
+
+const head = (/* array */) => {}; // Implementation here.
+//Tail
+//Implementa una función tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
+
+const tail = (/* array */) => {}; // Implementation here.
+
+//Init
+//Implementa una función init (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. Utiliza los métodos que ofrece Array.prototype.
+
+const init = (/* array */) => {}; // Implementation here.
+
+//Last
+//Implementa una función last (inmutable), tal que, dado un array como entrada devuelva el último elemento.
+
+const last = (/* array */) => {}; // Implementation here.
+
+//2. Concat
+//Implementa una función concat (inmutable) tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos. Utiliza rest / spread operators.
+
+const concat = (a, b) => {}; // Implementation here.
+
+//Opcional
+//Implementa una versión del ejercicio anterior donde se acepten múltiples arrays de entrada (más de 2).
+
---
+++ transpiled/01 deliverables/exercise-01.js
@@ -0,0 +1,35 @@
+console.log("************** DELIVERABLE 01 *********************");
+//1. Array operations
+//Head
+Implementa;
+una;
+función;
+head(inmutable), tal;
+que, dado;
+un;
+array;
+como;
+entrada;
+extraiga;
+y;
+devuelva;
+su;
+primer;
+elemento.Utiliza;
+destructuring.
+;
+var head = function ( /* array */) { }; // Implementation here.
+//Tail
+//Implementa una función tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
+var tail = function ( /* array */) { }; // Implementation here.
+//Init
+//Implementa una función init (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. Utiliza los métodos que ofrece Array.prototype.
+var init = function ( /* array */) { }; // Implementation here.
+//Last
+//Implementa una función last (inmutable), tal que, dado un array como entrada devuelva el último elemento.
+var last = function ( /* array */) { }; // Implementation here.
+//2. Concat
+//Implementa una función concat (inmutable) tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos. Utiliza rest / spread operators.
+var concat = function (a, b) { }; // Implementation here.
+//Opcional
+//Implementa una versión del ejercicio anterior donde se acepten múltiples arrays de entrada (más de 2).
